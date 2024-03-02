dd439497f5625611cfcc52e0bf1cf2370cc39e97
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-02 19:01:13 +0300

    IsPost Kavramı

---
+++ MVCCSharp/Controllers/IsPostEgController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCSharp.Controllers
+{
+    public class IsPostEgController : Controller
+    {
+        // GET: IsPostEg
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
