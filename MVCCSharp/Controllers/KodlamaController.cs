ef4ea0bba43c7e820bcc97df4444df9c7356df85
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-03 14:18:47 +0300

    HtmlEncode-HtmlDecode-HtmlRav

---
+++ MVCCSharp/Controllers/KodlamaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCSharp.Controllers
+{
+    public class KodlamaController : Controller
+    {
+        // GET: Kodlama
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
