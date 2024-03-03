a62f7c2f25eaa9dcbfae93eb07c44f6bc186c1b3
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-03 14:36:35 +0300

    String Formatlama

---
+++ MVCCSharp/Controllers/FormatlamaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCSharp.Controllers
+{
+    public class FormatlamaController : Controller
+    {
+        // GET: Formatlama
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
