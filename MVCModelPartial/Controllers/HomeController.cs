0b41bc419ab28dff49644388bb98c04326e896dd
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-03 22:16:41 +0300

    Model alan partialview oluşturma

---
+++ MVCModelPartial/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCModelPartial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Listele()
+        {
+            if (TempData["degerler"]==null)
+            {
+                ViewBag.Degerler = new List<int>();
+            }
+            else
+            {
+                ViewBag.Degerler = TempData["degerler"];
+
+            }
+
+            return View();
+        }
+        public ActionResult Doldur()
+        {
+            List<int> listDegerler=new List<int>();
+            listDegerler.Add(2);
+            listDegerler.Add(45);
+            listDegerler.Add(234);
+            listDegerler.Add(12);
+            listDegerler.Add(89);
+
+            TempData["degerler"]=listDegerler;
+            return RedirectToAction("Listele");
+
+        }
+    }
+}
---
+++ MVCModelPartial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCModelPartial.MvcApplication" Language="C#" %>
