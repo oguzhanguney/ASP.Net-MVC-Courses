dad29e22961be5b32b98936cc0aac470400d2317
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-03 15:05:02 +0300

    Uyarı mesajı oluşturma

---
+++ MVCCSharp/Controllers/UyarıMesajController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCSharp.Controllers
+{
+    public class UyarıMesajController : Controller
+    {
+        // GET: UyarıMesaj
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sayfa() //Yönlendirme sayfamızı oluşturma:
+        {
+            return View();
+        }
+    }
+}
