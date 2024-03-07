4bb9ecbab6f06747ea07b56d6ac8fe118aede974
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-07 14:17:25 +0300

    MVC attributes

---
+++ MVCAttribute/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MVCAttribute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAttribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult YeniVeriEkle()
+        {
+            //viewi oluştururken  modele veri ekleme işlemi yapacagımız için view-template i create olarak seçelim
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniVeriEkle(Uye uye)
+        {
+            return View();
+        }
+        //
+        private static Uye uye = new Uye()
+        {
+            Id = 1,
+            Ad = "Ali Osman",
+            Soyad = "Hazır",
+            TCKimlikNo = "11111111111",
+            UyelikTarihi = new DateTime(2017, 12, 09)
+        };
+        public ActionResult Detay()
+        {
+            return View(uye);
+        }
+    }
+}
---
+++ MVCAttribute/Models/Uye.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCAttribute.Models
+{
+    public class Uye
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Adınız")]//attributes2
+        public string Ad { get; set; }
+
+        [DisplayName("Soyadınız")]
+        public string Soyad { get; set; }
+
+        //Tckn nin mutlaka girilmesini istiyoruz.
+        //şimdi zorunluluk attribute ekleyelim. 
+        [Required]//attributes1
+        [DisplayName("Tc Kimlik Numarası")]
+        [MinLength(11,ErrorMessage ="Tc Kimlik No 11 hane olmak zorunda!")]//En az 11 karakter girilmeli. //artık deger tipi string olmalı double olamaz
+        [MaxLength(11,ErrorMessage = "Tc Kimlik No 11 hane olmak zorunda!")]//En fazla 11 karakter girilmeli.//attributes6
+        public string TCKimlikNo { get; set; }
+        
+        //bir detay sayfası yaparak tarihin nasıl göründügüne bakalım.
+        [DisplayFormat(DataFormatString ="{0:dd.MM.yyyy}")]//tarihin görüntüleme formatını ayarladık attributes3
+        [DisplayName("Üyelik Tarihi")]
+        public DateTime UyelikTarihi { get; set; }
+
+        [DataType(DataType.Password)]//attributes4
+        public string Parola { get; set; }
+        //Compare att bir başka prop ile karşılaştırma yapar-attributes7
+        [DataType(DataType.Password)]
+        [Compare("Parola")]
+        public string ParolaTekrar { get; set; }
+
+        [EmailAddress()]//attributes5
+        public string EPosta { get; set; }
+
+
+
+
+    }
+}
