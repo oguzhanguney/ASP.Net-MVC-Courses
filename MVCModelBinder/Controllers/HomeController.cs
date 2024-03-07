f469fd9e7e5eaccb5ed6dbab2a96bff93788e16b
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-07 14:44:53 +0300

    MVC model binding

---
+++ MVCModelBinder/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCModelBinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult YeniUrunEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrunEkle(string urunAd)
+        {   //yeniUrunEkle viewda birden fazla ürün girilebilirdi,bunları name att verdigimiz isimle home controllerda httppost ile yakalayıp
+            //metotun içerisinde istedigimiz gibi kullanabiliriz.bu işleme model binder deniyor.
+            return View();
+        }
+    }
+}
---
+++ MVCModelBinder/Models/Urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCModelBinder.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string urunAd { get; set; }
+    }
+}
