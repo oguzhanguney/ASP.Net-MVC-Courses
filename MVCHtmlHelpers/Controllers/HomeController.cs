4341470dfc8267a58f5a30d11972bfde630126c4
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-20 22:47:46 +0300

    MVC Html Helpers

---
+++ MVCHtmlHelpers/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            //html helperları bu view içerisinde nasıl kullanıldıgını inceleyelim.
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCHtmlHelpers/ReadMe.txt
@@ -0,0 +1,14 @@
+﻿
+Bu derste Mvc html helper konusunu işleyeceğiz.
+
+-Helper:
+	asp.net mvc de asp.net web formlarda oldugu gibi hazır kontroller yoktur.bunlarına yerine html kontrolleri kullanılır(select,input)
+	bunları biz sunucu taraflı kodlarla oluşturmak istersek helper metotlarından yararlarınız.4 çeşit helper var
+
+	1)Html Helper
+	2)Web Helper
+	3)microsoft helper
+	4)Rendering helper 
+
+	bunların yanında bizde kendi helper imizi oluşturabiliriz.
+	-home controller a gidip şimdi detaylıca inceleyelim.
