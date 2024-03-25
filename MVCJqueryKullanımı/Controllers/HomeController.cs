ba368d26d1bd7f4be2dcc2f6463b77bd49cc09a0
oguzhanguney <oguzhan.guney098@gmail.com>
2024-03-25 04:03:18 +0300

    index3 jquery hatası

---
+++ MVCJqueryKullanımı/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCJqueryKullanımı.Controllers
+{
+    public class HomeController : Controller
+    {
+        // değişken ve fonksiyon tanımlama:
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //buton click() olayı
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        //Dizi tanımlama ve each():
+        public ActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCJqueryKullanımı/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿Bu projede MVC ile Jquery kullanımını inceleyecegiz.
+
+	1)İlk olarak jquery ile değişken ve fonksiyon tanımalam işlemlerini yapalım.
+
+	2)buton click() olayını inceleyelim ve yeni bir actr tanımlayalım.
+
+	3)Jquery ile dizi tanımlama ve dizi içerisindeki elemanlara ulaşma
